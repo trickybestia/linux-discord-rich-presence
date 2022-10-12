d077d2d0be305dbc355331d960a9503e31b76ffd
trickybestia <trickybestia@gmail.com>
2022-10-12 07:27:02 +0300

    📝 Add another config template

---
+++ doc/configs/hide-presence-when-some-process-is-running.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/sh
+
+while true; do
+    cat <<EOF
+[ \
+    { \
+        "application_id": 0, \
+        "state": "$(uname -r)", \
+        "details": "$(uname -n)", \
+        "large_image": { \
+            "key": "some_image", \
+            "text": null \
+        }, \
+        "small_image": null, \
+        "start_timestamp": $(date -d "$(uptime -s)" +%s), \
+        "end_timestamp": null, \
+        "buttons": [ \
+            { \
+                "label": "some_button", \
+                "url": "https://example.com/" \
+            } \
+        ] \
+    } \
+]
+EOF
+
+    sleep 10
+
+    pids="$(pidof code) $(pidof wineserver)" # for example; you can put your own processes here
+
+    if [ "${#pids}" != 0 ]; then
+        echo "[]"
+
+        for pid in $pids; do
+            if [ -d "/proc/$pid" ]; then # if process haven't exited yet
+                tail --pid=$pid -f /dev/null # wait for process to exit
+            fi
+        done
+    fi
+done
